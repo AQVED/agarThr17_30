cbddfe6b81526eac7df1f53d01056268e127bd73
AQVED <78595838+AQVED@users.noreply.github.com>
2026-03-19 18:38:07 +0200

    Add files via upload

--- launcher.py
+++ launcher.py
@@ -1,41 +1,57 @@
+from tkinter import PhotoImage
+
 from customtkinter import *
 
-launcher = CTk()
-launcher.geometry("400x500")
-launcher.title("Agario Launcher")
+class ConnectWindow(CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("400x500")
+        self.title("Agario Launcher")
+        self.icon = PhotoImage(file="ico_mini.png")
+        self.wm_iconphoto(False, self.icon)
 
-heading = CTkLabel(launcher,text="AGARGARIO",font=("Arial",30,'bold'))
-heading.pack(pady=15)
+        self.username = None
+        self.host = None
+        self.port = None
 
-row1=CTkFrame(launcher,height=75)
-name_label = CTkLabel(row1, text="Ваш нікнейм",anchor='w')
-name_entry = CTkEntry(row1, placeholder_text="Нікнейм...")
+        heading = CTkLabel(self, text="AGARGARIO",font=("Arial",30,'bold'))
+        heading.pack(pady=15)
 
-name_label.pack(fill='x',padx=5)
-name_entry.pack(fill='x',padx=5)
+        row1=CTkFrame(self, height=75)
+        name_label = CTkLabel(row1, text="Ваш нікнейм",anchor='w')
+        self.name_entry = CTkEntry(row1, placeholder_text="Нікнейм...")
 
-row2=CTkFrame(launcher,height=75)
-host_label = CTkLabel(row2, text="IP серверу",anchor='w')
-host_entry = CTkEntry(row2, placeholder_text="IP...")
+        name_label.pack(fill='x',padx=5)
+        self.name_entry.pack(fill='x',padx=5)
 
-host_label.pack(fill='x',padx=5)
-host_entry.pack(fill='x',padx=5)
+        row2=CTkFrame(self,height=75)
+        host_label = CTkLabel(row2, text="IP серверу",anchor='w')
+        self.host_entry = CTkEntry(row2, placeholder_text="IP...")
 
-row3=CTkFrame(launcher,height=75)
-port_label = CTkLabel(row3, text="PORT серверу",anchor='w')
-port_entry = CTkEntry(row3, placeholder_text="PORT...")
+        host_label.pack(fill='x',padx=5)
+        self.host_entry.pack(fill='x',padx=5)
 
-port_label.pack(fill='x',padx=5)
-port_entry.pack(fill='x',padx=5)
+        row3=CTkFrame(self,height=75)
+        port_label = CTkLabel(row3, text="PORT серверу",anchor='w')
+        self.port_entry = CTkEntry(row3, placeholder_text="PORT...")
 
-row1.pack(pady=20, fill='x',padx=15)
-row1.pack_propagate(False)
-row2.pack(pady=20, fill='x',padx=15)
-row2.pack_propagate(False)
-row3.pack(pady=20, fill='x',padx=15)
-row3.pack_propagate(False)
+        port_label.pack(fill='x',padx=5)
+        self.port_entry.pack(fill='x',padx=5)
 
-connect_btn = CTkButton(launcher, text="Під'єднатись")
-connect_btn.pack(pady=15)
+        row1.pack(pady=20, fill='x',padx=15)
+        row1.pack_propagate(False)
+        row2.pack(pady=20, fill='x',padx=15)
+        row2.pack_propagate(False)
+        row3.pack(pady=20, fill='x',padx=15)
+        row3.pack_propagate(False)
 
-launcher.mainloop()
+        connect_btn = CTkButton(self, text="Під'єднатись")
+        connect_btn.pack(pady=15)
+
+    def open_game(self):
+        self.username = self.name_entry.get()
+        self.host = self.host_entry.get()
+        self.port = int(self.port_entry.get())
+        self.destroy()
+a=ConnectWindow()
+a.mainloop()
